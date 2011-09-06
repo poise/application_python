94dd04e3e396fc4d79363c94b1965c1f5fee2b8f
Noah Kantrowitz <noah@coderanger.net>
2011-09-05 21:24:28 -0700

    Rename application_django to application_python to reflect its broader utility.

---
+++ providers/django.rb
@@ -0,0 +1,120 @@
+#
+# Author:: Noah Kantrowitz <noah@opscode.com>
+# Cookbook Name:: application_django
+# Provider:: django
+#
+# Copyright:: 2011, Opscode, Inc <legal@opscode.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include Chef::Mixin::LanguageIncludeRecipe
+
+action :before_compile do
+
+  include_recipe 'python'
+
+  new_resource.migration_command "#{::File.join(new_resource.virtualenv, "bin", "python")} manage.py syncdb --migrate --noinput" if !new_resource.migration_command
+
+  new_resource.symlink_before_migrate.update({
+    new_resource.local_settings_base => new_resource.local_settings_file,
+  })
+end
+
+action :before_deploy do
+
+  python_virtualenv new_resource.virtualenv do
+    path new_resource.virtualenv
+    action :create
+  end
+
+  new_resource.packages.each do |name, ver|
+    python_pip name do
+      version ver if ver && ver.length > 0
+      virtualenv new_resource.virtualenv
+      action :install
+    end
+  end
+
+  dbm = new_resource.find_matching_role(new_resource.database_master_role)
+  Chef::Log.warn("No node with role #{new_resource.database_master_role}") if new_resource.database_master_role && !dbm
+
+  template "#{new_resource.path}/shared/#{new_resource.local_settings_base}" do
+    source new_resource.settings_template || "settings.py.erb"
+    cookbook new_resource.settings_template ? new_resource.cookbook_name : "application_django"
+    owner new_resource.owner
+    group new_resource.group
+    mode "644"
+    variables new_resource.settings.clone
+    variables.update :debug => new_resource.debug, :database => {
+      :host => (dbm.attribute?('cloud') ? dbm['cloud']['local_ipv4'] : dbm['ipaddress']),
+      :settings => new_resource.database,
+      :legacy => new_resource.legacy_database_settings
+    }
+  end
+
+end
+
+action :before_migrate do
+
+  if new_resource.requirements.nil?
+    # look for requirements.txt files in common locations
+    [
+      ::File.join(new_resource.path, "requirements", "#{node[:chef_environment]}.txt"),
+      ::File.join(new_resource.path, "requirements.txt")
+    ].each do |path|
+      if ::File.exists?(path)
+        new_resource.requirements path
+        break
+      end
+    end
+  end
+  if new_resource.requirements
+    Chef::Log.info("Installing using requirements file: #{new_resource.requirements}")
+    execute "pip install -E #{new_resource.virtualenv} -r #{new_resource.requirements}" do
+      cwd new_resource.release_path
+    end
+  else
+    Chef::Log.debug("No requirements file found")
+  end
+
+end
+
+action :before_symlink do
+
+  if new_resource.collectstatic
+    cmd = new_resource.collectstatic.is_a?(String) ? new_resource.collectstatic : "collectstatic --noinput"
+    execute "#{::File.join(new_resource.virtualenv, "bin", "python")} manage.py #{cmd}" do
+      user new_resource.owner
+      group new_resource.group
+      cwd new_resource.release_path
+    end
+  end
+
+  ruby_block "remove_run_migrations" do
+    block do
+      if node.role?("#{new_resource.application.name}_run_migrations")
+        Chef::Log.info("Migrations were run, removing role[#{new_resource.id}_run_migrations]")
+        node.run_list.remove("role[#{new_resource.id}_run_migrations]")
+      end
+    end
+  end
+
+end
+
+action :before_restart do
+end
+
+action :after_restart do
+end
+
---
+++ providers/gunicorn.rb
@@ -0,0 +1,92 @@
+#
+# Author:: Noah Kantrowitz <noah@opscode.com>
+# Cookbook Name:: application_django
+# Provider:: gunicorn
+#
+# Copyright:: 2011, Opscode, Inc <legal@opscode.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include Chef::Mixin::LanguageIncludeRecipe
+
+action :before_compile do
+
+  include_recipe "gunicorn"
+  include_recipe "supervisor"
+
+  if !new_resource.restart_command
+    new_resource.restart_command do
+      run_context.resource_collection.find(:supervisor_service => new_resource.application.name).run_action(:restart)
+    end
+  end
+
+  raise "You must specify an application module to load" unless new_resource.app_module
+
+end
+
+action :before_deploy do
+
+  new_resource = @new_resource
+
+  gunicorn_config "#{new_resource.application.path}/shared/gunicorn_config.py" do
+    action :create
+    template new_resource.settings_template || 'gunicorn.py.erb'
+    cookbook new_resource.settings_template ? new_resource.cookbook_name : 'gunicorn'
+    listen "#{new_resource.host}:#{new_resource.port}"
+    backlog new_resource.backlog
+    worker_processes new_resource.workers
+    worker_class new_resource.worker_class.to_s
+    #worker_connections
+    worker_max_requests new_resource.max_requests
+    worker_timeout new_resource.timeout
+    worker_keepalive new_resource.keepalive
+    #debug
+    #trace
+    preload_app new_resource.preload_app
+    #daemon
+    pid new_resource.pidfile
+    #umask
+    #logfile
+    #loglevel
+    #proc_name
+  end
+
+  supervisor_service new_resource.application.name do
+    action :enable
+    if new_resource.app_module == :django
+      django_resource = new_resource.application.sub_resources.select{|res| res.type == :django}.first
+      raise "No Django deployment resource found" unless django_resource
+      base_command = "#{::File.join(django_resource.virtualenv, "bin", "python")} manage.py run_gunicorn"
+    else
+      base_command = "gunicorn #{new_resource.app_module}"
+    end
+    command "#{base_command} -c #{new_resource.application.path}/shared/gunicorn_config.py"
+    directory ::File.join(new_resource.path, "current")
+    autostart false
+    user new_resource.owner
+  end
+
+end
+
+action :before_migrate do
+end
+
+action :before_symlink do
+end
+
+action :before_restart do
+end
+
+action :after_restart do
+end
