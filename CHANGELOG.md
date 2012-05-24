e42a9200eab94caf282eafc6a5ac09636cfb9a47
jtimberman <joshua@opscode.com>
2012-05-24 15:57:40 -0600

    release v1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## v1.0.0:
+
+* [COOK-1246] - Initial release - relates to COOK-634.
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Deploys and configures Python-based applications"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.99.12"
+version          "1.0.0"
 
 %w{ application python gunicorn supervisor }.each do |cb|
   depends cb
