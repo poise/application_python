bee3a8ef7b120f0d3ab932de11cb8c28b73a93d4
Noah Kantrowitz <noah@coderanger.net>
2015-10-03 17:41:26 -0700

    A bunch more tests.

--- test/spec/resources/django_spec.rb
+++ test/spec/resources/django_spec.rb
@@ -172,6 +172,46 @@ SETTINGS
       it { is_expected.to eq '/test/manage.py' }
     end # /describe #default_manage_path
 
+    describe '#default_settings_module' do
+      let(:settings_path) { nil }
+      subject { chef_run.application_django('/test').send(:default_settings_module) }
+      recipe(subject: false) do
+        application_django '/test'
+      end
+      before do
+        allow(chef_run.application_django('/test')).to receive(:find_file).with('settings.py').and_return(settings_path)
+      end
+
+      context 'with no settings.py' do
+        it { is_expected.to be_nil }
+      end # /context with no settings.py
+
+      context 'with simple settings.py' do
+        let(:settings_path) { '/test/myapp/settings.py' }
+        it { is_expected.to eq 'myapp.settings' }
+      end # /context with simple settings.py
+    end # /describe #default_settings_module
+
+    describe '#default_wsgi_module' do
+      let(:wsgi_path) { nil }
+      subject { chef_run.application_django('/test').send(:default_wsgi_module) }
+      recipe(subject: false) do
+        application_django '/test'
+      end
+      before do
+        allow(chef_run.application_django('/test')).to receive(:find_file).with('wsgi.py').and_return(wsgi_path)
+      end
+
+      context 'with no wsgi.py' do
+        it { is_expected.to be_nil }
+      end # /context with no wsgi.py
+
+      context 'with simple wsgi.py' do
+        let(:wsgi_path) { '/test/wsgi.py' }
+        it { is_expected.to eq 'wsgi' }
+      end # /context with simple wsgi.py
+    end # /describe #default_wsgi_module
+
     describe '#find_file' do
       let(:files) { [] }
       recipe(subject: false) do
@@ -208,4 +248,56 @@ SETTINGS
       end # /context with two matching files on the same level
     end # /describe #find_file
   end # /describe PoiseApplicationPython::Resources::Django::Resource
+
+  describe PoiseApplicationPython::Resources::Django::Provider do
+    step_into(:application_django)
+    context 'with default settings' do
+      recipe do
+        application_django '/test' do
+          # Hardwire all paths so it doesn't have to search.
+          manage_path 'manage.py'
+          settings_module 'myapp.settings'
+          wsgi_module 'wsgi'
+        end
+      end
+
+      it { is_expected.to run_python_execute('manage.py collectstatic --noinput') }
+      it { is_expected.to_not run_python_execute('manage.py syncdb --noinput') }
+      it { is_expected.to_not run_python_execute('manage.py migrate --noinput') }
+      it { is_expected.to render_file('/test/myapp/local_settings.py') }
+    end # /context with default settings
+
+    context 'with syncdb' do
+      recipe do
+        application_django '/test' do
+          # Hardwire all paths so it doesn't have to search.
+          manage_path 'manage.py'
+          settings_module 'myapp.settings'
+          syncdb true
+          wsgi_module 'wsgi'
+        end
+      end
+
+      it { is_expected.to run_python_execute('manage.py collectstatic --noinput') }
+      it { is_expected.to run_python_execute('manage.py syncdb --noinput') }
+      it { is_expected.to_not run_python_execute('manage.py migrate --noinput') }
+    end # /context with syncdb
+
+    context 'with migrate' do
+      recipe do
+        application_django '/test' do
+          # Hardwire all paths so it doesn't have to search.
+          manage_path 'manage.py'
+          migrate true
+          settings_module 'myapp.settings'
+          wsgi_module 'wsgi'
+        end
+      end
+
+      it { is_expected.to run_python_execute('manage.py collectstatic --noinput') }
+      it { is_expected.to_not run_python_execute('manage.py syncdb --noinput') }
+      it { is_expected.to run_python_execute('manage.py migrate --noinput') }
+      it { is_expected.to render_file('/test/myapp/local_settings.py') }
+    end # /context with migrate
+  end # /describe PoiseApplicationPython::Resources::Django::Provider
 end
